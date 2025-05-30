839811eb23eee7d322478abbbd8b706b4f68102c
vishal rathod <136430754+vishal-rathod-1@users.noreply.github.com>
2025-05-30 11:16:07 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,1 @@
-from flask import Flask
-
-app=Flask(__name__)
-
-@app.route('/')
-def home():
-  return "Hello DevSecOps"
-
-if __name__=="__main__"
-  app.run(PORT=8080)
+print("Hello Jenkins testing-1")
